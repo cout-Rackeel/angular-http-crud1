9ef0efd40f9de3d2353895dfedfd094790a399fe
rackeelBrooks <84402686+cout-Rackeel@users.noreply.github.com>
2022-06-21 15:34:31 -0500

    more styles

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -5,11 +5,12 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
+
+
 export class AboutComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(){}
 
 }
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  contacts:any[] = [
+    { id:1 , img:'' , adrs1:'Main Office Suite B11,Pinnacle Pointe,' , adrs2:'53 Lady Musgrave Rd, Kingston 10,' , country:'Jamaica', phn:'+1 876 616 7661'},
+
+    { id:2 , img:'' , adrs1:'Branch Office LGB Nagar 1st Street, Ramakrishnapuram, Sivanandhapuram,' , adrs2:'Coimbatore, TamilNadu, 641035,' , country:'India' , phn:'+91 96773 05900'},
+
+    { id:3 , img:'' , adrs1:'Branch Office 4 Daventry Road, Kaaimans Building 5th Floor,' , adrs2:'Lynnwood BridgeOffice Park, Lynnwood Manor, 0081 South Africa,' , country:' South Africa.',
+    phn:'  +27 12 483 8600'}
+  ]
+
+
+
   constructor() { }
 
   ngOnInit(): void {
